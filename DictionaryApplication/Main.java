1c5fe67ded7a35e575ee552671ca0f0715adc163
quangdeptrai2662000 <72980141+quangdeptrai2662000@users.noreply.github.com>
2020-12-18 11:44:56 +0700

    Add files via upload

---
+++ DictionaryApplication/Main.java
@@ -0,0 +1,30 @@
+package DictionaryApplication;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("sample4.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Quang Dictionary");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
